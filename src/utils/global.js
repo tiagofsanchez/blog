2ccaf700d7cc8e6305377d99aabc4b6290d25c7e
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-12-19 23:50:37 +0800

    FEATURE: git edit

---
+++ src/utils/global.js
@@ -0,0 +1,19 @@
+import urljoin from "url-join";
+import config from "../../data/SiteConfig";
+import moment from "moment";
+
+const URLify = string => {
+  return string.trim().replace(/\s/g, "%20");
+};
+
+const editOnGithub = post => {
+  const date = moment.utc(post.date).format(config.dateFromFormat);
+  const slug = URLify(post.title);
+  return urljoin(
+    config.repo,
+    "/tree/master/content/posts",
+    `${date}-${slug}.md`
+  );
+};
+
+export { editOnGithub };
