364d47906ff629f12e9ae12d0f66d403f2f74f2e
tiagofsanchez <tiagofsanchez@users.noreply.github.com>
2020-02-20 14:36:55 +0800

    FIX: bug with cover

--- gatsby-config.js
+++ gatsby-config.js
@@ -167,7 +167,6 @@ module.exports = {
                     }
                     frontmatter {
                       title
-                      cover
                       date
                       category
                       tags
